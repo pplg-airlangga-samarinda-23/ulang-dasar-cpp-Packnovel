740329d4df101ec936375baee3ed9225d65dbce8
lalu adam <adamlalu22@gmail.com>
2024-07-31 11:35:32 +0800

    selesai tgs

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
-    return 0;
+void cetakdata(string nama, string nisn, string jurusan, string tinggibadan)
+{
+    cout << "masukan nisn anda : " << endl;
+    cin >>nisn;
+    cout << "masukan nama anda : " << endl;
+    cin >>nama;
+    cout << "masukan jurusan anda : " << endl;
+    cin >>jurusan;
+    
+    cout << "tinggibadan : " << endl;
+    cin >>tinggibadan;
+
+
 }
+
+int main(){
+    string nama = "adam";
+    string jurusan = "PPLG";
+    string nisn = "uknown";
+    string tinggibadan = "160cm";
+
+
+   cetakdata(nama,nisn,jurusan,tinggibadan);
+
+   return 0; 
+}
+
